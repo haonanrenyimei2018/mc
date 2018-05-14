3b7d733f568d59a926313b6c3f6b8d130b6bd0e8
haonanrenyimei2018 <513958343@qq.com>
2018-05-14 22:37:03 +0800

    201805142235
    增加功能:
    代理广告，删除不需要的控制器
    增加代理列表的控制器

--- application/admin/controller/Ad.php
+++ application/admin/controller/Ad.php
@@ -9,7 +9,34 @@
 namespace app\admin\controller;
 
 
+use think\Db;
+
 class Ad extends Base
 {
 
+    public function index() {
+        $val = input('key');
+        $where = [];
+        if(isset($val) && !empty($val)) {
+            $where['title'] = ['like' , '%'.$val.'%'];
+        }
+        $Nowpage = input('get.page') ? input('get.page'):1;
+        $limits = 10;// 获取总条数
+        $count = Db::name('ad')->where($where)->count();//计算总页面
+        $allpage = intval(ceil($count / $limits));
+        if(input('get.page')){
+            $lists = Db::name('ad')->where($where)->page($Nowpage, $limits)->select();
+            $adTypes = Db::name('adType')->column('id,name');
+            $members = Db::name('agency')->column('id,name');
+            foreach ($lists as &$val) {
+                $val['type_name'] = $adTypes[$val['type']];
+                $val['member_name'] = $members[$val['mid']];
+            }
+            return json($lists);
+        }
+        $this->assign('Nowpage', $Nowpage); //当前页
+        $this->assign('allpage', $allpage); //总页数
+        $this->assign('val',$val);
+        return $this->fetch();
+    }
 }
---
+++ application/admin/controller/Member.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018\5\14 0014
+ * Time: 22:30
+ */
+
+namespace app\admin\controller;
+
+
+class Member extends Base
+{
+    /**
+     * 全部的会员
+     */
+    public function index() {
+        $key = input('key');
+
+
+
+    }
+    /**
+     * 待审核的会员
+     */
+    public function unchecked() {
+
+    }
+    /**
+     * 审核会员
+     */
+    public function check() {
+
+    }
+
+
+
+
+
+}
